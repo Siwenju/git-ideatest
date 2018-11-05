3f92f74b3c702a7154368af6b7f6e66edd601b8b
Si <1542365350@qq.com>
2018-11-05 19:41:09 +0800

    尔特让他

---
+++ git-test2/src/cn/itcast/HelloGit.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hahahahahah");
+        System.out.println("hahahahahah");
+        System.out.println("hahahahahah");
+    }
+    public void test(){
+        System.out.println("yyyyyy");
+    }
+}
